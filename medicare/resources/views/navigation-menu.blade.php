03509057a0cf56cf2e6967529dda865e27273775
jaylinkz <johnsonjefferson64@gmail.com>
2022-04-23 13:25:22 +0100

    second commit

--- medicare/resources/views/navigation-menu.blade.php
+++ medicare/resources/views/navigation-menu.blade.php
@@ -15,6 +15,10 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                            {{ __('Tasks') }}
+                        </x-jet-nav-link>
 
                 @can('client')
                 <x-jet-nav-link href="{{ route('main') }}" :active="request()->routeIs('main')">
